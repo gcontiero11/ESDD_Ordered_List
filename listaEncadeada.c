7476fbdbed4ff7509c13b2ee7d814c9713a66b96
gcontiero11 <gustcontiero@gmail.com>
2024-03-17 18:54:42 -0300

    ajeitando ponteiros

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -15,36 +15,36 @@ int allocateList(int size){
 }
 
 // SIZE MANAGER
-int reallocSize(t_sorted_list list){
-    list.elements = realloc(list.elements,sizeof(int) * (list.size + ((list.size/2) * list.reallocCount)));
-    list.reallocCount++;
+int reallocSize(t_sorted_list* list){
+    list->elements = realloc(list->elements,sizeof(int) * (list->size + ((list->size/2) * list->reallocCount)));
+    list->reallocCount++;
     return 1;
 }
 
 // ADD ELEMENTS
-int insertElement(t_sorted_list list, int elem){
+int insertElement(t_sorted_list* list, int elem){
     int index = findPosition(list,elem);
     freeIndexPosition(list,index);
     return 1;
 }
 
-int findPosition(t_sorted_list list,int elem){
+int findPosition(t_sorted_list* list,int elem){
     
 }
 
-int freeIndexPosition(t_sorted_list list, int index){
-    if (list.numberOfElements ==  list.size){
+int freeIndexPosition(t_sorted_list* list, int index){
+    if (list->numberOfElements ==  list->size){
         reallocSize(list);
     }
     int aux;
-    for (int i = list.numberOfElements - 1; i > index; i++){
+    for (int i = list->numberOfElements - 1; i > index; i++){
 
     }
     
 } 
 
-int updateValue(t_sorted_list list, int elem, int index){
-    list[index] = elem;
+int updateValue(t_sorted_list* list, int elem, int index){
+    list->elements[index] = elem;
     return list;
 }
 
@@ -55,45 +55,45 @@ void pop(){
 
 }
 
-void remove_by_index(t_sorted_list list,int index){
+void remove_by_index(t_sorted_list* list,int index){
     
 }
 
-void remove_by_element(t_sorted_list list,int elem){
+void remove_by_element(t_sorted_list* list,int elem){
 
 }
 
-void clear(t_sorted_list list){
+void clear(t_sorted_list* list){
     clear(list);
     return 1;
 }
 
 // LIST INFOS
-void get(t_sorted_list list,int index){
-    if(index > list.numberOfElements) return 0;
-    for(int count = 0; count < list.numberOfElements; count++){
+void get(t_sorted_list* list,int index){
+    if(index > list->numberOfElements) return 0;
+    for(int count = 0; count < list->numberOfElements; count++){
         
     }
 }
 
-void indexOf(t_sorted_list list,int elem){
+void indexOf(t_sorted_list* list,int elem){
     int index = 0;
-    for(int i = 0; i < list.numberOfElements; i++){
-        if(list[i] == elem) return index;
+    for(int i = 0; i < list->numberOfElements; i++){
+        if(list->elements[i] == elem) return index;
         index++;
     }
     return index;
 }
 
-void size(t_sorted_list list){
+void size(t_sorted_list* list){
 
 }
 
-void is_Empty(t_sorted_list list){
+void is_Empty(t_sorted_list* list){
 
 }
 
-void count(t_sorted_list list,int elem){
+void count(t_sorted_list* list,int elem){
 
 }
 
